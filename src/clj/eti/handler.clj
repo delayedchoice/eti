579096ec87c0b36ff9f4563c7e3a55bec4fcf7d3
Bobby Harris <bobby.harris@gmail.com>
2017-03-06 23:39:31 -0500

    cleaned up

--- src/clj/eti/handler.clj
+++ src/clj/eti/handler.clj
@@ -21,8 +21,7 @@
 
 (defn get-body-from-stream [resp]
   (let [body (resp :body)
-        len (-> resp :headers :content-length Integer/parseInt)
-        _ (log/debug (str "length: " len))]
+        len (-> resp :headers :content-length Integer/parseInt)]
     (with-open [rdr body]
      (let [buf (byte-array len)]
        (.read rdr buf)
@@ -51,33 +50,24 @@
 (defn load-from-cache [req]
   (let [hit (<!! (k/get-in store [(build-path-and-query-string req)]))]
     (if hit
-      (merge {:source :cache} hit)
-      nil)))
-
+        (merge {:source :cache} hit)
+        nil)))
 
 (defroutes proxy-route
   (rfn req
-       (let [
-             _ (log/debug (str "incoming request: " req))
-             out-req {:method (:request-method req)
+       (let [out-req {:method (:request-method req)
                       :url (build-url "localhost" "3000" req)
-                      ;:headers (dissoc (:headers req) "host" "content-length")
-                      ;:body (if-let [len (get-in req [:headers "content-length"])]
-                      ;        (slurp-binary (:body req) (Integer/parseInt len)))
                       :follow-redirects true
                       :throw-exceptions false
-                      :as :stream}
-             ;_ (log/debug (str "out-req: " out-req))
+                      :as :stream }
              response (some identity (lazy-seq [(load-from-cache req)
                                                 @(http/request out-req)]))
-
-             _ (log/debug (str "response: " response))
-             _ (log/debug "printed response")
-             ]
-         (let [body (get-body response)
-               response (assoc response :body body)
-               _ (log/debug "dbresponse: " (str (<!! (k/assoc-in store [(build-path-and-query-string req)] response ) )) )]
-           body))))
+						 body (get-body response)
+             response (assoc response :body body)
+             cache-response (<!! (k/assoc-in store [(build-path-and-query-string req)] response))
+             _ (log/debug "response: " response)
+             _ (log/debug "cache-response: " cache-response)]
+           body)))
 
 (def dev-handler (-> #'proxy-route
                      wrap-reload
@@ -86,5 +76,4 @@
                      #_(wrap-trace :header :ui)))
 
 (def handler (-> proxy-route
-                wrap-multipart-params
-                ))
+                wrap-multipart-params))
